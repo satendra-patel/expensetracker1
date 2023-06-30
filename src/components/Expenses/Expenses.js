b46194e8ee00dfc2694c785223bcd54d555cf618
satendra-patel <patelsatendra06@gmail.com>
2023-06-30 23:21:25 +0530

    logout button

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,9 +3,14 @@ import { Link } from 'react-router-dom'
 
 
 export default function Expenses() {
+  const logOutHandler=()=>{
+    localStorage.clear('idToken');
+    localStorage.clear('email');
+  }
   return (
     <div>
       <nav>
+        <Link to='/login'><button onClick={logOutHandler}>Logout</button></Link>
         <span className='px-4'>Welcome To Expense Tracker</span>
       <span className='float-end px-4'> Your Profile is incomplete . <Link to='/profile'>Complete Now!</Link></span>
       <br />
