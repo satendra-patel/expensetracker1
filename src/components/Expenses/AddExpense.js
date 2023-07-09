9ee679c5ebadf8c355bb678ddec7a2599ee6874e
satendra-patel <patelsatendra06@gmail.com>
2023-07-09 20:28:07 +0530

    with add and delete expense on database

--- src/components/Expenses/AddExpense.js
+++ src/components/Expenses/AddExpense.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import {
   MDBBtn,
   MDBContainer,
@@ -8,24 +8,91 @@ import {
   MDBCardBody,
   MDBInput,
 } from "mdb-react-ui-kit";
+import axios from "axios";
 
 export default function AddExpense() {
   const [expense, setexpense] = useState([]);
+  const [edit, setedit] = useState(false);
+  const [id, setid] = useState("");
+
   const categoryvalue = useRef();
   const pricevalue = useRef();
   const desvalue = useRef();
-  const submithandler = (event) => {
-    event.preventDefault();
-    const details = {
-      price: pricevalue.current.value,
-      des: desvalue.current.value,
-      cat: categoryvalue.current.value,
-    };
-    setexpense([...expense, details]);
-    pricevalue.current.value = "";
-    desvalue.current.value = "";
-    categoryvalue.current.value = "";
+
+  const editexpense = (price, des, cat, idvalue) => {
+    setedit(true);
+    setid(idvalue);
+
+    pricevalue.current.value = price;
+    desvalue.current.value = des;
+    categoryvalue.current.value = cat;
   };
+  const submithandler = async (event) => {
+    event.preventDefault();
+
+    if (edit) {
+      const details = {
+        price: pricevalue.current.value,
+        des: desvalue.current.value,
+        cat: categoryvalue.current.value,
+      };
+      const response = await axios.put(
+        `https://expense-tracker-a7536-default-rtdb.firebaseio.com/expenses/${id}.json`,
+        details
+      );
+      if (response.status === 200) {
+        console.log("Expense successfuly Updated");
+        setedit(false);
+      }
+      pricevalue.current.value = "";
+      desvalue.current.value = "";
+      categoryvalue.current.value = "";
+    } else {
+      const details = {
+        price: pricevalue.current.value,
+        des: desvalue.current.value,
+        cat: categoryvalue.current.value,
+      };
+      axios.post(
+        "https://expense-tracker-a7536-default-rtdb.firebaseio.com/expenses.json",
+        details
+      );
+      setexpense([...expense, details]);
+      pricevalue.current.value = "";
+      desvalue.current.value = "";
+      categoryvalue.current.value = "";
+    }
+  };
+  const deleteexpense = async (id) => {
+    setid(id);
+    const respose = await axios.delete(
+      `https://expense-tracker-a7536-default-rtdb.firebaseio.com/expenses/${id}.json`
+    );
+    if (respose.status === 200) {
+      console.log("SUCCESSFULLY DELETE EXPENSE");
+    }
+  };
+  useEffect(() => {
+    async function myfun() {
+      const data = await axios.get(
+        "https://expense-tracker-a7536-default-rtdb.firebaseio.com/expenses.json?print=pretty"
+      );
+      const respose = data.data;
+
+      const trasformData = [];
+      for (const key in respose) {
+        trasformData.push({
+          id: key,
+          price: respose[key].price,
+          des: respose[key].des,
+          cat: respose[key].cat,
+        });
+      }
+      setexpense(trasformData);
+    }
+    myfun();
+  }, [submithandler, deleteexpense]);
+
   return (
     <form onSubmit={submithandler}>
       <MDBContainer fluid>
@@ -68,12 +135,13 @@ export default function AddExpense() {
                     <option value="Fuel">Fuel</option>
                     <option value="Movie">Movie</option>
                     <option value="Travel">Travel</option>
+                    <option value="Other">Other</option>
                   </select>
                 </div>
                 <br />
 
                 <MDBBtn className="mb-4" size="lg" type="submit">
-                  Add Expense
+                  {edit ? "update" : "Add Expense"}
                 </MDBBtn>
               </MDBCol>
 
@@ -83,12 +151,18 @@ export default function AddExpense() {
                 className="order-1 order-lg-2 d-flex align-items-center"
               >
                 <table class="table">
-            {expense.length===0 && <h3 className="text-center my-2">No Expense Found Please Add</h3>}
+                  {expense.length === 0 && (
+                    <h3 className="text-center my-2">
+                      No Expense Found Please Add
+                    </h3>
+                  )}
                   <thead class="table-dark">
                     <tr>
                       <th scope="col">Price</th>
                       <th scope="col">Descrpition</th>
                       <th scope="col">Category</th>
+                      <th scope="col">edit</th>
+                      <th scope="col">Delete</th>
                     </tr>
                   </thead>
 
@@ -98,15 +172,42 @@ export default function AddExpense() {
                         <td>{item.price}</td>
                         <td>{item.des}</td>
                         <td>{item.cat}</td>
+                        <td>
+                          {
+                            <i
+                              className="fas fa-pen"
+                              style={{ cursor: "pointer" }}
+                              onClick={() => {
+                                editexpense(
+                                  item.price,
+                                  item.des,
+                                  item.cat,
+                                  item.id
+                                );
+                              }}
+                            />
+                          }
+                        </td>
+                        <td>
+                          {
+                            <i
+                              className="fas fa-trash"
+                              style={{ cursor: "pointer" }}
+                              onClick={() => {
+                                deleteexpense(item.id);
+                              }}
+                            />
+                          }
+                        </td>
                       </tr>
                     ))}
                   </tbody>
                 </table>
 
-                {/* <MDBCardImage
+                <MDBCardImage
               src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
               fluid
-            /> */}
+            />
               </MDBCol>
             </MDBRow>
           </MDBCardBody>
